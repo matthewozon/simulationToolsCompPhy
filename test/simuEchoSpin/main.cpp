4a8eb5800c5ad3fa99d67b38f67054432d6126e0
matthew <matthew.ozon@gmail.com>
2016-06-06 22:16:27 +0200

    spin echo: example of simulation

---
+++ test/simuEchoSpin/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#include "C_matrix.h"
+
+#include "PP_el_hahn.h"
+
+//using namespace std;
+
+int main()
+{
+    PP_el_hahn spin;
+    spin.Bz0=1.0;
+    spin.Bxy=0.02;
+    spin.gamma=1000;
+    spin.T1=0.5; spin.T2=2.0; spin.tw=Pi/(2.0*spin.gamma*spin.Bxy); spin.TE=30.0*spin.tw;
+    double dw=1.0*spin.gamma*spin.Bz0/100.0;
+    spin.muz0 = 1.0;//1.0*(1.0+(dw/(spin.gamma*spin.Bz0))); the shift is computed in the methods of PP_el_hahn
+    spin.DW=spin.gamma*spin.Bz0/20.0;
+
+    spin.D = 0.0*0.0000005;
+    spin.G = 1.0;//0.001;
+
+    //spin.monochromatic(0.0, 0.0, 1.0, 0.0*dw);
+    //spin.biChromatic(0.0, 0.0, 1.0, dw);
+    //spin.multiChrome(0.0, 0.0, 1.0, 200);
+    //spin.monochromaticDiffusion(0.0, 0.0, 1.0, 0.0*dw);
+    spin.multiChromeDiffusion(0.0, 0.0, 1.0, 200);
+    return 0;
+}
+
+
+
+//ok pour echo
+//PP_el_hahn spin;
+//spin.Bz0=1.0;
+//spin.Bxy=0.02;
+//spin.gamma=1000;
+//spin.T1=0.5; spin.T2=2.0; spin.tw=Pi/(2.0*spin.gamma*spin.Bxy); spin.TE=30.0*spin.tw;
+//double dw=1.0*spin.gamma*spin.Bz0/100.0;
+//spin.muz0 = 1.0;//1.0*(1.0+(dw/(spin.gamma*spin.Bz0))); the shift is computed in the methods of PP_el_hahn
+//spin.DW=spin.gamma*spin.Bz0/10.0;
+
+////spin.monochromatic(0.0, 0.0, 1.0, 0.0*dw, true);
+////spin.biChromatic(0.0, 0.0, 1.0, dw, true);
+//spin.multiChrome(0.0, 0.0, 1.0, 1000, true);
